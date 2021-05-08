e977cb4b02bd336e76f896638a5aa5226e783d87
Fishbite <43952420+Fishbite@users.noreply.github.com>
2021-05-08 13:05:43 +0100

    Event listenrs for control buttons & play pads complete, however, the event listeners for the play pads may be better done with event bubbling rather than having individual listenrs as they do at the moment

--- scripts/app.js
+++ scripts/app.js
@@ -2,13 +2,14 @@
 let gameSeq = []; // hold the sequence of flashing lights in an array
 let playerSeq = []; // keep track of player order: Game / Player
 let numFlash; // keep track of number of flashes
-let turn; // keep track of the turn we are on (score number)
+let turn = 0; // keep track of the turn we are on (score number)
 let ok; // track if player is doing well
-let gTurn; // computer's turn true / flase
+let compTurn; // computer's turn true / false
 let intervalID; // ID for setInterval
-let strict; // strict mode enabled true / false
-let sound; // sound true / false
-let power; // power on? true / false
+let strict = false; // strict mode enabled true / false
+let sound = true; // sound true / false
+let power = false; // power on? true / false
+let playing = false;
 let win; // did the player reproduce the complete sequence true / false
 
 // ************ control button selectors ************
@@ -22,4 +23,127 @@ const bottomLeft = document.getElementById("bottom-left");
 const bottomRight = document.getElementById("bottom-right");
 // ************ display selector ************
 const score = document.getElementById("score");
-console.log(score);
+
+// ************ event listeners ************
+powerBtn.addEventListener("click", (event) => {
+  if (powerBtn.checked) {
+    power = true;
+    score.innerText = "0";
+  } else {
+    power = false;
+    score.innerText = "";
+  }
+});
+strictBtn.addEventListener("change", (event) => {
+  if (strictBtn.checked) {
+    strict = true;
+  } else {
+    strict = false;
+  }
+});
+
+playBtn.addEventListener("change", (event) => {
+  if (playBtn.checked) {
+    playing = true;
+    if (power || win) {
+      play();
+    }
+  } else {
+    playing = false;
+    resetDefaults();
+    powerBtn.checked = false;
+    console.log("Stopped! Defaults reset");
+  }
+});
+// These are the event listeners for the pads
+// try re-writing using event bubbling to listen
+// to the 'pads' section wrapping the pads
+topLeft.addEventListener("click", (event) => {
+  console.log("power", power, "playing", playing);
+  if (power && playing) {
+    playerSeq.push(1);
+    console.log(playerSeq);
+    check();
+    one();
+    if (!win) {
+      console.log("No Win :(");
+      setTimeout(() => {
+        clearColour();
+      }, 250);
+    }
+    console.log(playerSeq);
+  } else if (power && !playing) {
+    console.log("stopped. play button not active");
+  }
+});
+topRight.addEventListener("click", (event) => {
+  console.log("power", power, "playing", playing);
+  if (power && playing) {
+    playerSeq.push(2);
+    console.log(playerSeq);
+    check();
+    one();
+    if (!win) {
+      console.log("No Win :(");
+      setTimeout(() => {
+        clearColour();
+      }, 250);
+    }
+    console.log(playerSeq);
+  } else if (power && !playing) {
+    console.log("stopped. play button not active");
+  }
+});
+bottomLeft.addEventListener("click", (event) => {
+  console.log("power", power, "playing", playing);
+  if (power && playing) {
+    playerSeq.push(3);
+    console.log(playerSeq);
+    check();
+    one();
+    if (!win) {
+      console.log("No Win :(");
+      setTimeout(() => {
+        clearColour();
+      }, 250);
+    }
+    console.log(playerSeq);
+  } else if (power && !playing) {
+    console.log("stopped. play button not active");
+  }
+});
+bottomRight.addEventListener("click", (event) => {
+  console.log("power", power, "playing", playing);
+  if (power && playing) {
+    playerSeq.push(4);
+    console.log(playerSeq);
+    check();
+    one();
+    if (!win) {
+      console.log("No Win :(");
+      setTimeout(() => {
+        clearColour();
+      }, 250);
+    }
+    console.log(playerSeq);
+  } else if (power && !playing) {
+    alert("You needs to press the play button before you can start");
+    console.log("stopped. play button not active");
+  }
+});
+
+function play() {
+  console.log("playing");
+}
+function check() {}
+function clearColour() {}
+function resetDefaults() {
+  win = false;
+  gameSeq = [];
+  playerSeq = [];
+  numFlash = 0;
+  intervalID = 0;
+  turn = 0;
+  score.innerText = "0";
+  ok = true;
+}
